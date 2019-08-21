a6aa6d3331212099a93cbb25de116501cf11f148
Ramarduk <ramarduk@gmail.com>
2019-08-21 17:41:31 -0600

    fixed token

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(helmet());
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
-  if (!authToken || authToken.split(' ')[1] !== apiToken.split(' ')[1]) {
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
   }
   next();
